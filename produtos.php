b6260cca7746ab13553578a5b3c4d610ae94773f
Tulio Macedo <tuliomac@gmail.com>
2014-07-07 22:50:47 -0200

    ajustes nos links e rotas

---
+++ produtos.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+    <h3>Produtos</h3>
+    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+    <p><a href="#" class="btn btn-primary btn-large">Saiba mais &raquo;</a></p>
+</div>
